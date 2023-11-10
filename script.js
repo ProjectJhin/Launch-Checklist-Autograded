64f12d0c415192cbb6bddca1076aa02d36a043f2
ProjectJhin <142424408+ProjectJhin@users.noreply.github.com>
2023-11-10 10:40:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 
 window.addEventListener("load", function() {
 
-    let listedPlanets;
+    document.getElementById("inputForm").addEventListener("submit", function(event) {
+        let list = [];
+        formSubmission(document, list, document.getElementsByName("pilotName")[0].value, document.getElementsByName("copilotName")[0].value, document.getElementsByName("fuelLevel")[0].value, document.getElementsByName("cargoMass")[0].value);
+    });
+    debugger;
+    let listedPlanets = myFetch();
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = listedPlanets;
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let ans = pickPlanet(listedPlanets);
+        addDestinationInfo(document, ans.name, ans.diameter, ans.star, ans.distance, ans.moons, ans.image);
     })
     
  });
